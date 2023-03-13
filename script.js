9c370a7224fc9f4e1b929c96acc6531ac06e9c9e
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-03-13 11:08:47 -0700

    Cleaned up comments for presentation

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,10 +6,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+//Event listener for generate button
 generateBtn.addEventListener("click", writePassword);
 
 
@@ -24,12 +22,10 @@ function generatePassword(){
   let password = "";
   console.log("Button Clicked"); //For dev visual
 
-  // I am presented with a series of prompts for password criteria
-  // THEN I select which criteria to include in the password
+  //Informs user of process
   alert("A new password will be generated based on your answers to the following questions.");
   
-  // WHEN prompted for the length of the password
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
+  // Conditional for obtaining length of password
   var passwordLength = prompt("Please enter a number value between 8-128 for the length of your password.");{
     while (passwordLength > 128 || passwordLength < 8){
       passwordLength = prompt("Please enter a valid number between 8-128.");
@@ -37,35 +33,26 @@ function generatePassword(){
     console.log(passwordLength);
   }
   
-  // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+  //Confirmation of desired character types
   var upperCase = confirm("Click 'Okay' to include uppercase letters.")
     console.log(upperCase); //For dev visual
-    //If else statement to determine inclusion of uppercase
-
 
   var lowerCase = confirm("Click 'Okay' to include lowercase letters.")
     console.log(lowerCase); //For dev visual
-  //If else statement to determine inclusion of lowercase
   
   var numbers = confirm("Click 'Okay' to include numbers.")
     console.log(numbers); //For dev visual
-  //If else statement to determine inclusion of numbers
   
   var specialChars = confirm("Click 'Okay' to include special characters.");
     console.log(specialChars); //For dev visual
-  //If else statement to determine inclusion of special chars
 
-
-
-
-  
-  // THEN my input should be validated and at least one character type should be selected
-  // If statements to verify the user's input. If they have chosen to use one of the selected types, that respective character set will be added to the array pool.
+  //Function for obtaining random value from a string
   function getCharacter(str) {
     const randomValue = Math.floor(Math.random() * str.length);
     return str[randomValue];
   }
   
+  //Conditionals for verifying requested character types are included
   if(upperCase){
     password = password + getCharacter(upperCaseSet);
   }
@@ -79,6 +66,7 @@ function generatePassword(){
     password = password + getCharacter(specialSet);
   }
 
+  //Conditionals for completing the remaining spots in the password
   let passSet = "";
 
   if(upperCase) {
@@ -100,6 +88,6 @@ function generatePassword(){
     password = password + getCharacter(passSet);
   }
 
-  // THEN the password is either displayed in an alert or written to the page//
+  //Returns password for display
   return password;
 }
